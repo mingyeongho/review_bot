0fee9355c1b3f3470908b5463515e8f5cd193b70
민경호 <1736s@naver.com>
2024-08-21 22:28:20 +0900

    design: 헤더 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-import { ModeToggle } from "@/src/widgets";
-import Image from "next/image";
-
 export default function Home() {
   return <main></main>;
 }
--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,12 +1,15 @@
+import Link from "next/link";
 import { ModeToggle } from "../ModeToggle";
 
 export function Header() {
   return (
-    <header className="flex justify-between items-center h-12 px-6">
+    <header className="w-full max-w-screen-lg m-auto flex justify-between items-center h-16 px-6 py-2 border-b border-slate-400">
       <div className="empty" />
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl blackHanSans">
-        ㄹㅂㅂ
-      </h1>
+      <Link href="/">
+        <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl font-blackHanSans">
+          ㄹㅂㅂ
+        </h1>
+      </Link>
       <ModeToggle />
     </header>
   );
